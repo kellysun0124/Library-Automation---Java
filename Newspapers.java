6051639f9f32fdafe11eaa40ac02e065f08072fa
NoahMarchbanks <noahmarchbanks2021@gmail.com>
2023-04-05 21:13:50 -0500

    Newspapers subclass (Noah, Bosco, Ben)

---
+++ Newspapers.java
@@ -0,0 +1,16 @@
+public class Newspapers extends Collections {
+    protected String ISBN;
+    protected String author;
+    
+    public Newspapers(String collectionID, String section, String type, String title, String publisher, int year, String genre, String ISBN, String author) {
+        super(collectionID, section, type, title, publisher, year, genre);
+    }
+
+    public String getISBN() {
+        return this.ISBN;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+}
