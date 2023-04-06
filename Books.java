8993e7cb43c222c8d70632036782547aa66c45ed
NoahMarchbanks <noahmarchbanks2021@gmail.com>
2023-04-05 21:14:56 -0500

    removed author from superclass to relevant subclas

--- Books.java
+++ Books.java
@@ -1,11 +1,16 @@
 public class Books extends Collections {
     protected String ISBN;
+    protected String author;
 
-    public Books(String collectionID, String author, String section, String type, String title, String publisher, int year, String genre, String ISBN) {
-        super(collectionID, author, section, type, title, publisher, year, genre);
+    public Books(String collectionID, String section, String type, String title, String publisher, int year, String genre, String ISBN, String author) {
+        super(collectionID, section, type, title, publisher, year, genre);
     }
 
     public String getISBN() {
         return this.ISBN;
+    }
+
+    public String getAuthor() {
+        return this.author;
     }
 }
