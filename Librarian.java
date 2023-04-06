c8b2ca0ef2f18b232124aeccb403aa5b75cce314
Kelly Sun <kellysun0124@gmail.com>
2023-04-05 21:11:35 -0500

    Update Librarian.java
    changed access names to get, and added setMemberID

--- Librarian.java
+++ Librarian.java
@@ -2,8 +2,11 @@ public class Librarian extends Employee {
     public Librarian (String name, String address, String dateOfBirth, String email, String socialSecurityNumber, int employeeNumber){
         super(name, address, dateOfBirth, email, socialSecurityNumber, employeeNumber);
     }
-    public void accessMember(Member member){
+    public void getMemberID(Member member){
         member.getMemberID();
+    }
+    public void setMemberID(Member member){
+        member.setMemberID();
     }
 }
 
