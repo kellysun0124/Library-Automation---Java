ec37d533bc4d5399ee9c2062ec60bb25264f8b76
CzeschinW <83725039+CzeschinW@users.noreply.github.com>
2023-04-05 21:14:52 -0500

    adding collection compatibility

--- External.java
+++ External.java
@@ -4,4 +4,16 @@ public class External extends Member {
         super(name, address, dateOfBirth, email, socialSecurityNumber, memberID);
         
     }
+    
+    private Collections[] collectionsArray = new Collections[5];
+    private int collectionCount = 0;
+    
+    public void addCollection(Collections collection) {
+        if (collectionCount < 5) {
+            collectionsArray[collectionCount] = collection;
+            collectionCount++;
+        } else {
+            System.out.println("Error: Maxium collections reached.");
+        }
+    }
 }
