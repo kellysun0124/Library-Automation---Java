6f605dffa58dc8d78046208046c5d2c4ec488b33
NoahMarchbanks <noahmarchbanks2021@gmail.com>
2023-04-12 18:58:22 -0500

    Constructor overhaul, defaults and overloadeds

--- Books.java
+++ Books.java
@@ -1,10 +1,31 @@
+import java.util.*;
+
 public class Books extends Collections {
     protected String ISBN;
     protected String author;
     protected int amountOfPages;
 
-    public Books(String collectionID, String section, String type, String title, String publisher, int year, String genre, String ISBN, String author, int amountOfPages) {
-        super(collectionID, section, type, title, publisher, year, genre);
+    public Books() {
+        super();
+        this.type = "Book";
+        this.ISBN = "000000";
+        this.author = "Unknown Author";
+        this.amountOfPages = 0;
+    }
+
+    public Books(String collectionID, String title, String ISBN) {
+        this();
+        this.collectionID = collectionID;
+        this.title = title;
+        this.ISBN = ISBN;
+    }
+
+    public Books(String collectionID, String section, String title, String publisher, String genre, Calendar datePublished, String ISBN, String author, int amountOfPages) {
+        super(collectionID, section, title, publisher, genre, datePublished);
+        this.type = "Book";
+        this.ISBN = ISBN;
+        this.author = author;
+        this.amountOfPages = amountOfPages;
     }
 
     public String getISBN() {
--- Journals.java
+++ Journals.java
@@ -1,10 +1,31 @@
+import java.util.*;
+
 public class Journals extends Collections {
     protected String ISSN;
     protected String author;
     protected int amountOfPages;
 
-    public Journals(String collectionID, String section, String type, String title, String publisher, int year, String genre, String ISSN, String author, int amountOfPages) {
-        super(collectionID, section, type, title, publisher, year, genre);
+    public Journals() {
+        super();
+        this.type = "Journal";
+        this.ISSN = "000000";
+        this.author = "Unknown Author";
+        this.amountOfPages = 0;
+    }
+
+    public Journals(String collectionID, String title, String ISSN) {
+        this();
+        this.collectionID = collectionID;
+        this.title = title;
+        this.ISSN = ISSN;
+    }
+
+    public Journals(String collectionID, String section, String title, String publisher, String genre, Calendar datePublished, String ISSN, String author, int amountOfPages) {
+        super(collectionID, section, title, publisher, genre, datePublished);
+        this.type = "Journal";
+        this.ISSN = ISSN;
+        this.author = author;
+        this.amountOfPages = amountOfPages;
     }
 
     public String getISSN() {
