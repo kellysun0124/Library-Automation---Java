fa26e8c9e1e0b431d37dea686e2a24296804966b
Kelly Sun <kellysun0124@gmail.com>
2023-04-16 16:40:31 -0500

    added set and get membertype

--- Member.java
+++ Member.java
@@ -1,10 +1,20 @@
 public class Member extends Person {
     private int memberID;
+    private String memberType;
 
     // Constructor
-    public Member(String name, String address, String dateOfBirth, String email, String socialSecurityNumber, int memberID) {
+    public Member(String name, String address, String dateOfBirth, String email, String socialSecurityNumber, int memberID, String memberType) {
         super(name, address, dateOfBirth, email, socialSecurityNumber);
         this.memberID = memberID;
+        this.memberType = memberType;
+    }
+
+    public String getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(String memberType) {
+        this.memberType = memberType;
     }
 
     // Getter and Setter for memberID
