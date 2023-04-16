56838f10e2f92243e8a6904d9be1c9bab9c1ec40
Bennclark2002 <92816199+Bennclark2002@users.noreply.github.com>
2023-04-16 17:27:35 -0500

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,17 +1,4 @@
-public class Employee extends Person {
-    private int employeeNumber;
 
-    // Constructor
-    public Employee(String name, String address, String dateOfBirth, String email, String socialSecurityNumber, int employeeNumber) {
-        super(name, address, dateOfBirth, email, socialSecurityNumber);
-        this.employeeNumber = employeeNumber;
-    }
+public class Employee {
 
-    // Getter and Setter for employeeNumber
-    public int getEmployeeNumber() {
-        return employeeNumber;
-    }
-    public void setEmployeeNumber(int employeeNumber) {
-        this.employeeNumber = employeeNumber;
-    }
 }
