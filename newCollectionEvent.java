f01160207f75ad3f12a638c4c24c57a85d36723e
Kelly Sun <kellysun0124@gmail.com>
2023-04-16 18:24:44 -0500

    Merge branch 'main' of https://github.com/kellysun0124/Library-Automation---Java-OOP into kelly

--- newCollectionEvent.java
+++ newCollectionEvent.java
@@ -4,14 +4,4 @@ import java.io.*;
 
 public class newCollectionEvent {
     
-    
-
-
-    protected String section;
-    protected String collectionID;
-    protected String type;
-    protected String publisher;
-    protected String title;
-    protected String genre;
-    protected Calendar datePublished;
 }
