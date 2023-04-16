1fd08f84fe3e7e2dcda512c8849664a3dfe8c3be
Kelly Sun <kellysun0124@gmail.com>
2023-04-16 18:02:54 -0500

    Update newMemberEvent.java

--- newMemberEvent.java
+++ newMemberEvent.java
@@ -31,7 +31,6 @@ public class newMemberEvent {
         System.out.print("Creating a new member...");
         Member mem = new Member(name,address,dob,email,ssn,memberID,membertype);
 
-        
 
         mem.saveTo("membershipdatabasefile.txt");
         System.out.println("New Member Successfully Saved to file."); 
