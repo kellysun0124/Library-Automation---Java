fb5ee289477f4bccae08b3f6c819dd12cc832e6c
Kelly Sun <kellysun0124@gmail.com>
2023-04-16 18:23:56 -0500

    Update newCollectionEvent.java
    don't look at it lol

--- newCollectionEvent.java
+++ newCollectionEvent.java
@@ -4,4 +4,14 @@ import java.io.*;
 
 public class newCollectionEvent {
     
+    
+
+
+    protected String section;
+    protected String collectionID;
+    protected String type;
+    protected String publisher;
+    protected String title;
+    protected String genre;
+    protected Calendar datePublished;
 }
