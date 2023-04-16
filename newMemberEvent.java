53d3f0dab0633b57d65ce5b31ed3aeacb882a991
Kelly Sun <kellysun0124@gmail.com>
2023-04-16 18:11:17 -0500

    Update newMemberEvent.java
    try? catch???

--- newMemberEvent.java
+++ newMemberEvent.java
@@ -4,36 +4,40 @@ import java.io.*;
 public class newMemberEvent {
     
     public static void newMemberEvent(){
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Enter Membership info: ");
+        try{
+            Scanner scn = new Scanner(System.in);
+            System.out.println("Enter Membership info: ");
 
-        System.out.print("Enter Member Name: ");
-        String name = scn.nextLine();
+            System.out.print("Enter Member Name: ");
+            String name = scn.nextLine();
 
-        System.out.print("Enter Member Address: ");
-        String address = scn.nextLine();
+            System.out.print("Enter Member Address: ");
+            String address = scn.nextLine();
 
-        System.out.print("Enter Member Date of Birth: ");
-        String dob = scn.nextLine();
+            System.out.print("Enter Member Date of Birth: ");
+            String dob = scn.nextLine();
 
-        System.out.print("Enter Member email: ");
-        String email = scn.nextLine();
+            System.out.print("Enter Member email: ");
+            String email = scn.nextLine();
 
-        System.out.print("Enter Member SSN: ");
-        String ssn = scn.nextLine();
+            System.out.print("Enter Member SSN: ");
+            String ssn = scn.nextLine();
 
-        System.out.print("Enter Member ID: ");
-        String memberID = scn.nextLine();        
+            System.out.print("Enter Member ID: ");
+            String memberID = scn.nextLine();        
 
-        System.out.print("Enter Membership type (Student/Professor/External): ");
-        String membertype = scn.nextLine();
+            System.out.print("Enter Membership type (Student/Professor/External): ");
+            String membertype = scn.nextLine();
 
-        System.out.print("Creating a new member...");
-        Member mem = new Member(name,address,dob,email,ssn,memberID,membertype);
+            System.out.print("Creating a new member...");
+            Member mem = new Member(name,address,dob,email,ssn,memberID,membertype);
 
+
+
+            mem.saveTo("membershipdatabasefile.txt");
+            System.out.println("New Member Successfully Saved to file."); 
+        } catch (Exception e){
         
-
-        mem.saveTo("membershipdatabasefile.txt");
-        System.out.println("New Member Successfully Saved to file."); 
+        }
     }
 }
